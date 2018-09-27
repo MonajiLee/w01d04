a5215976d2d4d16e98797f27e1bca0a0d2225fa8
MonajiLee <lee.smonica@gmail.com>
2018-09-27 22:41:19 +0000

    Created .sort function by name alphabetically and then descending age

--- custom-comparator.js
+++ custom-comparator.js
@@ -4,3 +4,23 @@ var students = [
     { id: 3, name: "alex",     age: 22 },
     { id: 4, name: "alex",     age: 30 }
   ];
+
+  // Say we have a list of students that we would like to sort in alphabetical order. 
+  // If students have the same name, then we prioritize the older student first.
+
+students.sort(function(a, b){
+    if (a.name < b.name) {
+        return -1
+    } else if (a.name > b.name) {
+        return 1
+    } else if (a.name = b.name) {
+        return b.age - a.age
+    }
+});
+
+console.log(students);
+
+
+//   // Write a sorting function (sometimes called a custom comparator), that sorts the above 
+//   // array first by the name ascending alphabetically, and in cases where the names are 
+//   // equal sort by descending age.
